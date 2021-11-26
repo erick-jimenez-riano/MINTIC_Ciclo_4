a6f9cd5e610a238430bd44cd17e675e85b183667
erick-jimenez-riano <erjimenezri@unal.edu.co>
2021-11-26 08:59:01 -0500

    Actualizacion

--- src/app/psicologos/psicologos.module.ts
+++ src/app/psicologos/psicologos.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CitasComponent } from './citas/citas.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CitasComponent
+  ],
   imports: [
     CommonModule
   ]
